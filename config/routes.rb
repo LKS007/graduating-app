a8308ee814919ccd2b4519c75230a3d49a0afe77
LKS007 <leon99_21@mail.ru>
2016-06-30 16:34:41 +0300

    add login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'signup'  => 'users#new' 
   resources :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 end
