29462307776f13f9b66a363919f236ba50478ae4
LKS007 <leon99_21@mail.ru>
2016-07-11 18:39:08 +0300

    action mailer

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,3 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "LKS007@yandex.ru"
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,20 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      tls: true,
+      address: 'smtp.yandex.ru',
+      port: 465,
+      domain: 'yandex.ru',
+      authentication: 'plain',
+      user_name: 'LKS007@yandex.com',
+      password: 'q32norm',
+      enable_starttls_auto: true
+  }
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
